380f84f11942e9809eb6434bc8be8d88ad30444d
osechi3 <holypoetwrk@gmail.com>
2020-09-03 21:40:06 +0300

    Removing player property from class Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,12 @@
 import PubSub from 'pubsub-js'
 
 export class Player {
-  constructor (player = 'player', gameboard) {
-    this.player = player
+  constructor (gameboard) {
     this.gameboard = gameboard
   }
 
   makeTurn (x, y) {
     PubSub.publish('turn_is_made', {
-      player: this.player,
       x: x,
       y: y
     })
