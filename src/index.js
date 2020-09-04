624307fd8fbe5d2cfd945703520a043a4dfa1ad6
osechi3 <holypoetwrk@gmail.com>
2020-09-04 22:33:17 +0300

    Creating the start of the game loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css'
+import { Player } from './components/Player'
+import { Gameboard } from './components/Gameboard'
+import { View } from './components/View'
+
+class Game {
+  static init () {
+    const player1 = new Player(new Gameboard())
+    const player2 = new Player(new Gameboard())
+    player1.gameboard.initShips()
+    player2.gameboard.initShips()
+
+    View.init()
+    View.placeShips(player1, player2)
+  }
+}
+Game.init()
